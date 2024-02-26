af463e02692719d585d46ae0f75204e80c870ffc
kossev89 <122936075+kossev89@users.noreply.github.com>
2024-02-26 22:56:26 +0200

    Add project files.

---
+++ FixIt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FixIt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
