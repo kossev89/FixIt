943532b131108081e98b0523f7e00e85b31a825b
kossev89 <kossev89@gmail.com>
2024-02-27 15:09:49 +0200

    Adding Technician Entoty

---
+++ FixIt.Infrastructure/Data/Enumerators/TechnicianSpecialization.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixIt.Infrastructure.Data.Enumerators
+{
+    public enum TechnicianSpecialization
+    {
+        Electronics,
+        Engine,
+        Suspension,
+        Mechanics,
+        Body
+    }
+}
