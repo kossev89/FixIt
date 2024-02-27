0f59fb1e4f523d1e5d52e77eeeff08f7f4f7a768
kossev89 <kossev89@gmail.com>
2024-02-27 15:11:21 +0200

    Updating Appointment and Technician entities

--- FixIt.Infrastructure/Data/Models/Appointment.cs
+++ FixIt.Infrastructure/Data/Models/Appointment.cs
@@ -1,15 +1,9 @@
 ﻿using FixIt.Infrastructure.Data.Enumerators;
-using FixIt.Infrastructure.Data.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FixIt.Infrastructure.Data.Models
 {
--- FixIt.Infrastructure/Data/Models/Technician.cs
+++ FixIt.Infrastructure/Data/Models/Technician.cs
@@ -1,6 +1,25 @@
-﻿namespace FixIt.Infrastructure.Data.Models
+﻿using FixIt.Infrastructure.Data.Enumerators;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using static FixIt.Infrastructure.Data.Constants.ValidationConstants;
+
+namespace FixIt.Infrastructure.Data.Models
 {
+    [Comment("Technicians Table")]
     public class Technician
     {
+        [Comment("Technician Identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Technician Name")]
+        [Required]
+        [StringLength(TechnicianNameMax)]
+        public string Name { get; set; } = string.Empty;
+
+        [Comment("Technician Specialization")]
+        [Required]
+        public TechnicianSpecialization Specialization { get; set; }
     }
 }
