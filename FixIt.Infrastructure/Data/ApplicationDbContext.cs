30c548d6fcd6ce4cc8a2c8d8f6cf3adef881e830
kossev89 <kossev89@gmail.com>
2024-02-27 19:48:26 +0200

    Added custom user properties and updating DbContext

--- FixIt.Infrastructure/Data/ApplicationDbContext.cs
+++ FixIt.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FixIt.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FixIt.Infrastructure.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
---
+++ FixIt.Infrastructure/Data/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixIt.Infrastructure.Data.Models
+{
+    public class User: IdentityUser
+    {
+        [Comment("Cars, owned by the user")]
+        [Required]
+        public ICollection<Car> Cars { get; set; } = new List<Car>();
+    }
+}
