33702f577f708c8e7c3ab7d127f3116ad9861d1a
kossev89 <kossev89@gmail.com>
2024-02-27 15:27:03 +0200

    Just added the ServiceHistory. Not completed!

--- FixIt.Infrastructure/Data/Models/ServiceHistory.cs
+++ FixIt.Infrastructure/Data/Models/ServiceHistory.cs
@@ -1,6 +1,21 @@
-﻿namespace FixIt.Infrastructure.Data.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FixIt.Infrastructure.Data.Models
 {
+    [Comment("Table for the ServiceHistory")]
     public class ServiceHistory
     {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public Car Car { get; set; } 
     }
+
+//Id(Primary Key)
+//CarId(Foreign Key to associate the service history record with the car)
+//ServiceId(Foreign Key to specify the type of service)
+//TechnicianId(Foreign Key to specify the technician who performed the service)
+//Date
+//Mileage
+//Price
+//AdditionalDetails(e.g., Notes about the service)
 }
