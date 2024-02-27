1eb69b391d1c60684bbe021515a2df7786096fcb
kossev89 <kossev89@gmail.com>
2024-02-27 19:38:26 +0200

    Added entity ServiceTyoe

---
+++ FixIt.Infrastructure/Data/Enumerators/ServiceType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixIt.Infrastructure.Data.Enumerators
+{
+    public enum ServiceType
+    {
+        TireRotation,
+        OilChange,
+        Diagnostics,
+        EngineRepair,
+        SuspensionRepair,
+        BodyRepair,
+        Paint
+    }
+}
