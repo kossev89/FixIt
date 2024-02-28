b1db0a8894ccaaf3147d3badc3bb51f083d7dca6
kossev89 <122936075+kossev89@users.noreply.github.com>
2024-02-29 00:09:35 +0200

    Added new migration for changed entities. Updated database.

---
+++ FixIt.Infrastructure/Migrations/20240228220836_EntotiesModified.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FixIt.Infrastructure.Migrations
+{
+    public partial class EntotiesModified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Mileage",
+                table: "ServiceHistories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                comment: "Current car mileage");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Mileage",
+                table: "ServiceHistories");
+        }
+    }
+}
