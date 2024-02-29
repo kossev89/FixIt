e14c8bb8d23402c1977916d18fa3ef65532912c6
kossev89 <122936075+kossev89@users.noreply.github.com>
2024-02-29 23:30:05 +0200

    Added MVC skeleton

---
+++ FixIt/Controllers/CarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FixIt.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FixIt/Controllers/ServiceHistoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FixIt.Controllers
+{
+    public class ServiceHistoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
