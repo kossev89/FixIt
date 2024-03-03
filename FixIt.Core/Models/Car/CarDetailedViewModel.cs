e975604d373638fd8c646c44e0e1cf959dbf3200
kossev89 <122936075+kossev89@users.noreply.github.com>
2024-03-04 00:43:16 +0200

    Added CarDetailedView model

---
+++ FixIt.Core/Models/Car/CarDetailedViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace FixIt.Core.Models.Car
+{
+    /// <summary>
+    /// Detailed Model View for Car Entity
+    /// </summary>
+    public class CarDetailedViewModel
+    {
+        public int Id { get; set; }
+        public string Make { get; init; } = string.Empty;
+        public string Model { get; init; } = string.Empty;
+        public int Year { get; init; }
+        public string PlateNumber { get; init; } = string.Empty;
+        public string? Vin { get; init; }
+        public int Mileage { get; set; }
+        public string? ImageUrl { get; init; }
+    }
+}
